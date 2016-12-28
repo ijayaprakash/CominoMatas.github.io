79f6a1eb1c6edf7079be830119c9dd9811aea722
CominoMatas <cominomatas@gmail.com>
2016-12-28 05:25:17 +0100

    Add files via upload

---
+++ animated-menu-icon.js
@@ -0,0 +1,10 @@
+/*----------------------------------
+Icono menú animado
+--------------------------------*/
+
+
+$(document).ready(function(){
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+	});
+});
